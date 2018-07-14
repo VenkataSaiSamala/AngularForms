a33dee4c50b24f60c984c359129ba01046c796fd
venkata sai kumar samala <venkata_samala@outlook.com>
2018-07-13 21:41:42 -0500

    Initial Commit Overview

---
+++ AngularForms/app/DataService.js
@@ -0,0 +1,30 @@
+﻿
+angularFormsApp.factory('DataService',
+    function () {
+        var getEmployee = function (id) {
+            if (id == 123) {
+                return {
+                    fullName: "Venkat Sai",
+                    notes: "Some note",
+                    department: "Administration",
+                    perkCar: true,
+                    perkStock: false,
+                    perkSixWeeks: true,
+                    payrollType: "none"
+                };
+            }
+            return undefined;
+        };
+        var insertEmployee = function (newEmployee) {
+            return true;
+        };
+        var updateEmployee = function (employee) {
+            return true;
+        };
+
+        return {
+            insertEmployee: insertEmployee,
+            updateEmployee: updateEmployee,
+            getEmployee: getEmployee
+        };
+    });
---
+++ AngularForms/app/EmployeeForm/efController.js
@@ -0,0 +1,41 @@
+﻿
+angularFormsApp.controller('efController',
+    function efController($scope, $window, $routeParams, DataService) {
+
+        if ($routeParams.id)
+            $scope.employee = DataService.getEmployee($routeParams.id);
+        else
+            $scope.employee = { id: 0 };
+
+        //$scope.employee = DataService.employee;
+        $scope.editableEmployee = angular.copy($scope.employee);
+
+        $scope.departments = [
+            "Engineering",
+            "Marketing",
+            "Finanace",
+            "Administration"
+        ];
+
+        
+
+        $scope.submitForm = function () {
+
+            if ($scope.editableEmployee.id == 0) {
+                //$scope.employee = angular.copy($scope.editableEmployee);
+                //insert new employee
+                DataService.insertEmployee($scope.editableEmployee);
+            }
+            else {
+                //update the employee
+                DataService.updateEmployee($scope.editableEmployee);
+            }
+
+            $scope.employee = angular.copy($scope.editableEmployee);
+            $window.history.back();
+        };
+
+        $scope.resetForm = function () {
+            $window.history.back();
+        }
+    });
